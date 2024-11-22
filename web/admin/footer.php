7e03a2b95a3ba5958fdc79818deb5eaba1256d80
universekannan <universekannan@gmail.com>
2024-11-22 10:59:25 +0530

    Kannan PC

--- web/admin/footer.php
+++
@@ -1,6 +0,0 @@
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2011 <a style="color:yellow" href="https://galaxytechnologypark.com">Galaxy Technology Park Pvt Ltd</a>.</strong> All rights
-  </footer>
